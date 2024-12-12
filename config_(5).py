4e6f7b9f2cebd9d1790dfbfd6549eef6581cbad8
ultra2006 <gamerultra791@gmail.com>
2024-12-12 20:08:18 +0530

    Add files via upload

---
+++ config_(5).py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7763302446:AAEu2Ui11xIiFkP3hbLEmT0y64J5XGJVvGw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@SHYAM_hacker")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6192971829")
+
+
+
+
+
+
+
+  
