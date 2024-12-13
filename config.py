942cda1362feb1dc92d4c0757b55bc4c987a7d99
ultra2006 <gamerultra791@gmail.com>
2024-12-13 18:39:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 BOT_TOKEN=("7763302446:AAEu2Ui11xIiFkP3hbLEmT0y64J5XGJVvGw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@SHYAM_hacker")
+OWNER_USERNAME=("@ULTRA_GAMER_OP")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6192971829")
+ADMIN_IDS=("6135948216")
 
 
 
